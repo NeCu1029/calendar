2cc113690b0622fe36c2aab6975be7b6f8a618bd
NeCu1029 <necu1029@gmail.com>
2025-12-23 08:19:38 +0900

    initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+App = Flask(__name__)
+
+
+@App.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    App.run(port=1111, debug=True)
