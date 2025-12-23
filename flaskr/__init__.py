b39913b04dd27875710f38bdfb8df1abfdcc5a29
NeCu1029 <necu1029@gmail.com>
2025-12-24 08:18:04 +0900

    add group & authentication page

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -6,3 +6,18 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("auth/login.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("auth/register.html")
+
+
+@app.route("/group/<int:group_no>")
+def group(group_no: int):
+    return render_template("group.html", no=group_no)
