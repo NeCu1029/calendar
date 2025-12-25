fede59881faa06e5fe0e6cd4215199c0faade0cf
NeCu1029 <necu1029@gmail.com>
2025-12-25 23:07:09 +0900

    implement registration & login back-end

---
+++ README.md
@@ -0,0 +1,14 @@
+# 공유 캘린더
+
+## 필요한 라이브러리 (Python)
+
+- flask
+- flask-bcrypt
+- flask-login
+- flask-sqlalchemy
+- python-dotenv
+
+## 로컬 서버 실행 방법
+
+1. cmd에서 루트 디렉토리로 이동
+2. `py -m flask run` 실행
--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,22 +1,30 @@
+import os
 from flask import Flask, render_template
-import account
+from flask_login import LoginManager
+from .user import bcrypt, db, User, user_bp
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+
+bcrypt.init_app(app)
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+login_manager = LoginManager(app)
+login_manager.login_view = "user_bp.login"  # type: ignore
+
+app.register_blueprint(user_bp)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
-
-
-@app.route("/login")
-def login():
-    return render_template("auth/login.html")
-
-
-@app.route("/register")
-def register():
-    return render_template("auth/register.html")
 
 
 @app.route("/group/<int:group_no>")
