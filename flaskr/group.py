d52b042fd2fe43a040ddcb6bbc043f7f287c1d09
NeCu1029 <necu1029@gmail.com>
2026-02-23 01:06:45 +0900

    finish my-page ui

--- flaskr/group.py
+++ flaskr/group.py
@@ -72,6 +72,7 @@ def join():
 @group_bp.route("/my")
 @login_required
 def my():
+    flash("테스트 플래시")
     return render_template(
         "group/my.html",
         groups=users_group(current_user.id),
