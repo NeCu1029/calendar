ef8438c7fd27e3a230c658260d24f52b53718715
NeCu1029 <necu1029@gmail.com>
2026-02-23 18:58:34 +0900

    add eventclick

--- flaskr/static/js/my.js
+++ flaskr/static/js/my.js
@@ -1,8 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
   const calendar = document.querySelector(".calendar");
+  const aside = document.querySelector("aside");
   const fc = new FullCalendar.Calendar(calendar, {
     initialView: "dayGridMonth",
     events: "/api/get_user_sch",
+    eventClick: function (info) {
+      aside.innerHTML =
+        info.event.title + "<br />" + info.event.extendedProps.creator;
+      console.log(info.event);
+    },
   });
   fc.render();
 });
